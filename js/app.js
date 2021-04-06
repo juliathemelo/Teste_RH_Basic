8f545327d8ddd8e2680dbed0735a820a2a5ab18c
Julia De Melo <juliarexsteam@hotmail.com>
2021-04-06 14:19:54 -0300

    HomePage

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu')
+const toggleMenu = document.getElementById('nav-toggle')
+const closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+// MOVE IMAGE
+
+const card = document.getElementById("book")
+
+card.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+})
+
