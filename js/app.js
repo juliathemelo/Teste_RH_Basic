ee8979cc790c9f313c3ccf6eccd71772e5321960
Julia De Melo <juliarexsteam@hotmail.com>
2021-04-07 15:20:59 -0300

    Finish

--- js/app.js
+++ js/app.js
@@ -23,24 +23,53 @@ card.addEventListener("mousemove", (e) => {
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 })
 
-//Slider Person
 
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-});
 
 //Função personagem 
 
-function Carlos(){
+
+function Carlos(p){
+    const imagem = window.document.getElementById("apareca")
     const carlos = "Carlos"
     const local = window.document.getElementById("escolha").innerHTML = `Personagem Selecionado: ${carlos}`
     window.alert(`${carlos} - Foi Selecionado, Prossiga as etapas`)
+    imagem.src = "pixels/carlos2.png"
+    imagem.style.width = "200px"
+    p = "Carlos"
     
 }
 
+function Pedro(p){
+    const imagem = window.document.getElementById("apareca")
+    const pedro = "Pedro"
+    const local = window.document.getElementById("escolha").innerHTML = `Personagem Selecionado: ${pedro}`
+    window.alert(`${pedro} - Foi Selecionado, Prossiga as etapas`)
+    imagem.src = "pixels/pedro2.png"
+    imagem.style.width = "200px"
+    p = "Pedro"
+    
+}
+
+function Thomas(){
+    const imagem = window.document.getElementById("apareca")
+    const thomas = "Thomas"
+    const local = window.document.getElementById("escolha").innerHTML = `Personagem Selecionado: ${thomas}`
+    window.alert(`${thomas} - Foi Selecionado, Prossiga as etapas`)
+    imagem.src = "pixels/thomas.png"
+    imagem.style.width = "200px"
+    p = "Thomas"
+    
+}
+//Função enviar
+const envio = document.getElementById("enviar")
+const text = document.getElementById("titulo")
+envio.addEventListener("click", Enviar)
+function Enviar(){
+    var x = text.value
+    if(x == 0){
+        window.alert("Favor inserir o titulo")
+    }else{
+        window.alert(`Texto Enviado Para O Nosso Banco De Dados! Obrigada Candidato`)
+    }
+    
+}
